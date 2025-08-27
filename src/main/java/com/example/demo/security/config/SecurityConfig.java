a84204f2a1ca031225d215a6d00824b30f6feaf3
KUNALM17 <83109225+KUNALM17@users.noreply.github.com>
2025-08-27 10:24:32 +0530

    Change IP

--- src/main/java/com/example/demo/security/config/SecurityConfig.java
+++ src/main/java/com/example/demo/security/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // Dono address allow karo
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://34.131.31.90:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://34.131.243.228:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
